2b58c299c4a3100b2b937c1c47967f4259339ab2
zhaaaa7 <zhaaaaa7@gmail.com>
2019-06-14 13:55:24 -0700

    Updates

---
+++ precache-manifest.01dd4db5fb9a514dab7e03b4319f1d42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65e25894ab95d74fe10d23ad907a83f",
+    "url": "/flagdream/index.html"
+  },
+  {
+    "revision": "6c0d8ed0cf2c4f024b27",
+    "url": "/flagdream/static/css/main.77ec0d01.chunk.css"
+  },
+  {
+    "revision": "aa162ea5d98f86d459af",
+    "url": "/flagdream/static/js/2.fc10a1fe.chunk.js"
+  },
+  {
+    "revision": "6c0d8ed0cf2c4f024b27",
+    "url": "/flagdream/static/js/main.8f8b5b60.chunk.js"
+  },
+  {
+    "revision": "bb51003e8c589483ff20",
+    "url": "/flagdream/static/js/runtime~main.08bff271.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flagdream/precache-manifest.4fc0c39d1e2d5adc15859696dbd381f2.js"
+  "/flagdream/precache-manifest.01dd4db5fb9a514dab7e03b4319f1d42.js"
 );
 
 self.addEventListener('message', (event) => {
